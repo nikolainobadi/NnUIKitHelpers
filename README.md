85a2f8680f307e6a09b648873a83ce44aa5303ec
Nikolai Nobadi <nikolai.rivas@gmail.com>
2022-02-09 16:05:47 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NnUIKitHelpers
+
+A description of this package.
---
+++ Sources/NnUIKitHelpers/NnUIKitHelpers.swift
@@ -0,0 +1,6 @@
+public struct NnUIKitHelpers {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
