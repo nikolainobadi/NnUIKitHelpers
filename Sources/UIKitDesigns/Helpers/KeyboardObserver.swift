1ea4896e6b768d51b054b22cca631e0d51275ef5
Nikolai Nobadi <nikolai.rivas@gmail.com>
2022-02-09 16:57:05 -0800

    Added NnView, NnViewController and KeyboardObserver

---
+++ Sources/UIKitDesigns/Helpers/KeyboardObserver.swift
@@ -0,0 +1,47 @@
+//
+//  KeyboardObserver.swift
+//  
+//
+//  Created by Nikolai Nobadi on 2/9/22.
+//
+
+import UIKit
+import Combine
+
+public final class KeyboardObserver {
+    
+    // MARK: - Properties
+    private let notifCenter = NotificationCenter.default
+    
+    
+    // MARK: - Init
+    public init() { }
+}
+
+
+// MARK: - Keyboard Observer
+extension KeyboardObserver {
+    
+    public var keyboardHeightPublisher: AnyPublisher<CGFloat, Never> {
+        Publishers.Merge(willShowPublisher, willHidePublisher)
+            .eraseToAnyPublisher()
+    }
+}
+
+
+// MARK: - Private
+private extension KeyboardObserver {
+    
+    var willShowPublisher: AnyPublisher<CGFloat, Never> {
+        notifCenter.publisher(for: UIResponder.keyboardWillShowNotification)
+            .compactMap { $0.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue }
+            .map { $0.cgRectValue.height }
+            .eraseToAnyPublisher()
+    }
+    
+    var willHidePublisher: AnyPublisher<CGFloat, Never> {
+        notifCenter.publisher(for: UIResponder.keyboardWillHideNotification)
+            .map { _ in CGFloat(0) }
+            .eraseToAnyPublisher()
+    }
+}
---
+++ Sources/UIKitDesigns/NnView.swift
@@ -0,0 +1,45 @@
+//
+//  NnView.swift
+//  
+//
+//  Created by Nikolai Nobadi on 2/9/22.
+//
+
+import UIKit
+
+open class NnView: UIView {
+    
+    // MARK: - Init
+    public init(frame: CGRect = .zero, color: UIColor? = .systemBackground) {
+        super.init(frame: frame)
+        
+        addSubviews()
+        backgroundColor = color
+    }
+    
+    @available(*, unavailable, message: "Can't load from a nib, fool!")
+    required public init?(coder: NSCoder) {
+        fatalError("Can't load from a nib, fool!")
+    }
+    
+    // MARK: - Display Setup
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layoutIfNeeded()
+        setupConstraints()
+    }
+    
+    open func addSubviews() { }
+    open func setupConstraints() { }
+    
+    public func addShadowEffect(cornerRadius: CGFloat) {
+        layer.cornerRadius = cornerRadius
+        layer.shadowRadius = 4
+        layer.masksToBounds = false
+        layer.shadowOpacity = 1.0
+        layer.shadowOffset = CGSize(width: 1, height: 2.0)
+    }
+}
+
+
