8640ed5086a36e957d377c6bb629ca36f54743cd
Nikolai Nobadi <nikolai.rivas@gmail.com>
2022-02-09 17:47:37 -0800

    Added TextFieldToolBar

---
+++ Sources/NnUIKitHelpers/UIKitDesigns/TextFieldToolBar.swift
@@ -0,0 +1,68 @@
+//
+//  TextFieldToolBar.swift
+//  
+//
+//  Created by Nikolai Nobadi on 2/9/22.
+//
+
+import UIKit
+
+public final class TextFieldToolBar: UIToolbar {
+    
+    // MARK: - Properties
+    private let onNextTapped: () -> Void
+    private let onCancelTapped: () -> Void
+
+    
+    // MARK: - Buttons
+    lazy var nextButton: UIBarButtonItem = {
+        UIBarButtonItem(title: "Next",
+                        style: .plain,
+                        target: self,
+                        action: #selector(didTapNextButton))
+    }()
+    
+    lazy var cancelButton: UIBarButtonItem = {
+        UIBarButtonItem(title: "Cancel",
+                        style: .plain,
+                        target: self,
+                        action: #selector(didTapCancelButton))
+    }()
+    
+    lazy var spaceButton: UIBarButtonItem = {
+        UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                        target: nil,
+                        action: nil)
+    }()
+    
+    
+    // MARK: - Init
+    init(onNextTapped: @escaping () -> Void,
+         onCancelTapped: @escaping () -> Void) {
+        
+        self.onNextTapped = onNextTapped
+        self.onCancelTapped = onCancelTapped
+        super.init(frame: .zero)
+        
+        sizeToFit()
+        tintColor = .white
+        setItems([cancelButton, spaceButton, nextButton],
+                 animated: false)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Actions
+    @objc func didTapNextButton() {
+        onNextTapped()
+    }
+    
+    @objc func didTapCancelButton() {
+        onCancelTapped()
+    }
+}
+
+
