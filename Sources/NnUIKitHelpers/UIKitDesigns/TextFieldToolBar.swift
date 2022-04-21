16a9d0bd712e31da67948b06c6cf1c5c0143279e
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-04-21 15:30:16 -0700

    updated TextFieldToolBar to be able to customize buttons

--- Sources/NnUIKitHelpers/UIKitDesigns/TextFieldToolBar.swift
+++ Sources/NnUIKitHelpers/UIKitDesigns/TextFieldToolBar.swift
@@ -7,23 +7,33 @@
 
 import UIKit
 
+struct ToolBarInfo {
+    var leftButtonTitle: String? = nil
+    var rightButtonTitle: String? = nil
+    var leftButtonAction: (() -> Void)? = nil
+    var rightButtonAction: (() -> Void)? = nil
+    var tint: UIColor? = nil
+}
+
 public final class TextFieldToolBar: UIToolbar {
     
     // MARK: - Properties
-    private let onNextTapped: (() -> Void)?
-    private let onCancelTapped: (() -> Void)?
+    private let rightButtonText: String
+    private let leftButtonText: String
+    private let rightButtonAction: (() -> Void)?
+    private let leftButtonAction: (() -> Void)?
 
     
     // MARK: - Buttons
     lazy var nextButton: UIBarButtonItem = {
-        UIBarButtonItem(title: "Next",
+        UIBarButtonItem(title: rightButtonText,
                         style: .plain,
                         target: self,
                         action: #selector(didTapNextButton))
     }()
     
     lazy var cancelButton: UIBarButtonItem = {
-        UIBarButtonItem(title: "Cancel",
+        UIBarButtonItem(title: leftButtonText,
                         style: .plain,
                         target: self,
                         action: #selector(didTapCancelButton))
@@ -37,19 +47,25 @@ public final class TextFieldToolBar: UIToolbar {
     
     
     // MARK: - Init
-    public init(onNextTapped: (() -> Void)? = nil,
+    public init(leftButtonText: String = "Cancel",
+                rightButtonText: String = "Next",
+                onNextTapped: (() -> Void)? = nil,
                 onCancelTapped: (() -> Void)? = nil,
                 tintColor: UIColor? = .white) {
         
-        self.onNextTapped = onNextTapped
-        self.onCancelTapped = onCancelTapped
+        self.leftButtonText = leftButtonText
+        self.rightButtonText = rightButtonText
+        self.leftButtonAction = onCancelTapped
+        self.rightButtonAction = onNextTapped
         super.init(frame: .zero)
         
-        sizeToFit()
+        self.sizeToFit()
         self.tintColor = tintColor
         
-        setItems([cancelButton, spaceButton, nextButton],
-                 animated: false)
+        let leftButton = onCancelTapped != nil ? cancelButton : spaceButton
+        let rightButton = onNextTapped != nil ? nextButton : spaceButton
+
+        setItems([leftButton, spaceButton, rightButton], animated: false)
     }
     
     required init?(coder: NSCoder) {
@@ -59,12 +75,10 @@ public final class TextFieldToolBar: UIToolbar {
     
     // MARK: - Actions
     @objc func didTapNextButton() {
-        onNextTapped?()
+        rightButtonAction?()
     }
     
     @objc func didTapCancelButton() {
-        onCancelTapped?()
+        leftButtonAction?()
     }
 }
-
-
